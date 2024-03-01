973061239339a1fc25acf4ffa9b71720911da8c4
fkodjo <franc.amouzouvi@gmail.com>
2024-03-01 14:39:49 +0000

    add api description

---
+++ src/config/swagger.py
@@ -0,0 +1,43 @@
+template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Bookmarks API",
+        "description": "API for bookmarks",
+        "contact": {
+            "responsibleOrganization": "",
+            "responsibleDeveloper": "",
+            "email": "email@gmail.com",
+            "url": "https://www.linkedin.com/in/kodjo-amouzouvi/",
+        },
+        "termsOfService": "https://www.linkedin.com/in/kodjo-amouzouvi/",
+        "version": "1.0"
+    },
+    "basePath": "/api/v1",  # base bash for blueprint registration
+    "schemes": [
+        "http",
+        "https"
+    ],
+    "securityDefinitions": {
+        "Bearer": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+            "description": "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\""
+        }
+    },
+}
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'apispec',
+            "route": '/apispec.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/"
+}
